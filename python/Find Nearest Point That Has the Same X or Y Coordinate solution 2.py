fe243fe4f8bfb0cdfcd6c9081acdab7b28e0a838
Etancher <30360093+jahanvir@users.noreply.github.com>
2022-07-28 14:22:58 +0530

    Create  Find Nearest Point That Has the Same X or Y Coordinate solution 2.py

---
+++ python/Find Nearest Point That Has the Same X or Y Coordinate solution 2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def nearestValidPoint(self, x: int, y: int, points: List[List[int]]) -> int:
+        minD=float('inf')
+        index=-1
+        for i in range(len(points)-1,-1,-1):
+            a,b=points[i]
+            if a==x or b==y:
+                d=abs(a-x)+abs(b-y)
+                if d<=minD:
+                    minD,index=d,i
+        return index
+        
+        
